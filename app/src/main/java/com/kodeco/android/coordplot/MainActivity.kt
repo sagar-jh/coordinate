5327e2c7c63d277b023760c45aa35ce8bffc68f1
sagar-jh <sagar@jhtechnologies.com>
2023-09-02 11:59:32 -0700

    Initial Commit

---
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.kodeco.android.coordplot
+
+import android.icu.lang.UCharacter.VerticalOrientation
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Slider
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                PlotSurface()
+            }
+        }
+    }
+}
+
+@Composable
+fun PlotSurface() {
+
+
+    var xPercent: Float by remember { mutableStateOf(0.5f) }
+    var yPercent: Float by remember { mutableStateOf(0.5f) }
+
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement =Arrangement.Center
+        ){
+                Box(Modifier.background(Color.Blue).size(300.dp)
+                ){
+                    Map(xPercent,yPercent)
+                }
+
+
+
+
+            createHorizontalSlider(value =xPercent,
+                changeValue={value ->
+                    xPercent=value
+                    }
+                )
+            createVerticalSlider(value =yPercent,
+                changeValue = { value ->
+                    yPercent = value
+                    //Log.i("y-tag", (yPercent * 300 - 18).toString())
+                }
+            )
+            //createVerticalSlider()
+        }
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PlotSurfacePreview() {
+    MyApplicationTheme {
+        PlotSurface()
+    }
+}
+
+@Composable
+fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
+
+    Box(
+        modifier
+            .size(36.dp)
+            .offset((xPercent * 300 - 18).dp, (yPercent * 300 - 18).dp)
+            .clip(shape = CircleShape)
+            .background(Color.Red)
+
+
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MapPreview() {
+    MyApplicationTheme {
+        Map(xPercent = 0.5f, yPercent = 0.5f)
+    }
+}
